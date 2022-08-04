7a6005ad1196c69b2502b50bc5420c20a29edeb9
Wout <wout@bittremieux.be>
2022-08-04 13:12:20 -0700

    Refactor the data loaders module

--- casanovo/denovo/dataloaders.py
+++ casanovo/denovo/dataloaders.py
@@ -1,45 +1,44 @@
-"""Data loaders for the de novo sequencing task.
-
-This module also ensure consistent train, validation, and test splits.
-"""
+import functools
 import os
-from functools import partial
+from typing import List, Optional, Tuple
 
-import torch
 import numpy as np
 import pytorch_lightning as pl
+import torch
+from depthcharge.data import AnnotatedSpectrumIndex
 
 from ..data import AnnotatedSpectrumDataset, SpectrumDataset
 
 
 class DeNovoDataModule(pl.LightningDataModule):
-    """Prepare data for a Spec2Pep.
+    """
+    Data loader to prepare MS/MS spectra for a Spec2Pep predictor.
 
     Parameters
     ----------
-    train_index : AnnotatedSpectrumIndex
-        The spectrum index file for training.
-    valid_index : AnnotatedSpectrumIndex
-        The spectrum index file for validation.
-    test_index : AnnotatedSpectrumIndex
-        The spectrum index file for testing.
-    batch_size : int, optional
-        The batch size to use for training and evaluations
-    n_peaks : int, optional
-        Keep only the top-n most intense peaks in any spectrum. ``None``
-        retains all of the peaks.
-    min_mz : float, optional
-        The minimum m/z to include. The default is 140 m/z, in order to
-        exclude TMT and iTRAQ reporter ions.
-    max_mz : float, optional
+    train_index : Optional[AnnotatedSpectrumIndex]
+        The spectrum index file corresponding to the training data.
+    valid_index : Optional[AnnotatedSpectrumIndex]
+        The spectrum index file corresponding to the validation data.
+    test_index : Optional[AnnotatedSpectrumIndex]
+        The spectrum index file corresponding to the testing data.
+    batch_size : int
+        The batch size to use for training and evaluating.
+    n_peaks : Optional[int]
+        The number of top-n most intense peaks to keep in each spectrum. `None` retains
+        all peaks.
+    min_mz : float
+        The minimum m/z to include. The default is 140 m/z, in order to exclude TMT and
+        iTRAQ reporter ions.
+    max_mz : float
         The maximum m/z to include.
-    min_intensity : float, optional
-        Remove peaks whose intensity is below `min_intensity` percentage
-        of the intensity of the most intense peak
-    fragment_tol_mass : float, optional
-        Fragment mass tolerance around the precursor mass in Da to remove the
-        precursor peak.
-    num_workers : int, optional
+    min_intensity : float
+        Remove peaks whose intensity is below `min_intensity` percentage of the base
+        peak intensity.
+    remove_precursor_tol : float
+        Remove peaks within the given mass tolerance in Dalton around the precursor
+        mass.
+    n_workers : int, optional
         The number of workers to use for data loading. By default, the number
         of available CPU cores on the current machine is used.
     random_state : int or Generator, optional.
@@ -49,19 +48,18 @@ class DeNovoDataModule(pl.LightningDataModule):
 
     def __init__(
         self,
-        train_index=None,
-        valid_index=None,
-        test_index=None,
-        batch_size=128,
-        n_peaks=200,
-        min_mz=140,
-        max_mz=2500,
-        min_intensity=0.01,
-        fragment_tol_mass=2,
-        num_workers=None,
-        random_state=None,
+        train_index: Optional[AnnotatedSpectrumIndex] = None,
+        valid_index: Optional[AnnotatedSpectrumIndex] = None,
+        test_index: Optional[AnnotatedSpectrumIndex] = None,
+        batch_size: int = 128,
+        n_peaks: Optional[int] = 150,
+        min_mz: float = 140.0,
+        max_mz: float = 2500.0,
+        min_intensity: float = 0.01,
+        remove_precursor_tol: float = 2.0,
+        n_workers: Optional[int] = None,
+        random_state: Optional[int] = None,
     ):
-        """Initialize the PairedSpectrumDataModule."""
         super().__init__()
         self.train_index = train_index
         self.valid_index = valid_index
@@ -71,36 +69,32 @@ class DeNovoDataModule(pl.LightningDataModule):
         self.min_mz = min_mz
         self.max_mz = max_mz
         self.min_intensity = min_intensity
-        self.fragment_tol_mass = fragment_tol_mass
-        self.num_workers = num_workers
+        self.remove_precursor_tol = remove_precursor_tol
+        self.n_workers = n_workers if n_workers is not None else os.cpu_count()
         self.rng = np.random.default_rng(random_state)
         self.train_dataset = None
         self.valid_dataset = None
         self.test_dataset = None
 
-        if self.num_workers is None:
-            self.num_workers = os.cpu_count()
-
-    def setup(self, stage=None, annotated=True):
-        """Set up the PyTorch Datasets.
+    def setup(self, stage: str = None, annotated: bool = True) -> None:
+        """
+        Set up the PyTorch Datasets.
 
         Parameters
         ----------
         stage : str {"fit", "validate", "test"}
-            The stage indicating which Datasets to prepare. All are prepared
-            by default.
+            The stage indicating which Datasets to prepare. All are prepared by default.
         annotated: bool
-            True if peptide sequence annotations available for test data
+            True if peptide sequence annotations are available for the test data.
         """
-
         if stage in (None, "fit", "validate"):
-            make_dataset = partial(
+            make_dataset = functools.partial(
                 AnnotatedSpectrumDataset,
                 n_peaks=self.n_peaks,
                 min_mz=self.min_mz,
                 max_mz=self.max_mz,
                 min_intensity=self.min_intensity,
-                fragment_tol_mass=self.fragment_tol_mass,
+                remove_precursor_tol=self.remove_precursor_tol,
             )
             if self.train_index is not None:
                 self.train_dataset = make_dataset(
@@ -109,31 +103,23 @@ class DeNovoDataModule(pl.LightningDataModule):
                 )
             if self.valid_index is not None:
                 self.valid_dataset = make_dataset(self.valid_index)
-
         if stage in (None, "test"):
-            if annotated == True:
-                make_dataset = partial(
-                    AnnotatedSpectrumDataset,
-                    n_peaks=self.n_peaks,
-                    min_mz=self.min_mz,
-                    max_mz=self.max_mz,
-                    min_intensity=self.min_intensity,
-                    fragment_tol_mass=self.fragment_tol_mass,
-                )
-            else:
-                make_dataset = partial(
-                    SpectrumDataset,
-                    n_peaks=self.n_peaks,
-                    min_mz=self.min_mz,
-                    max_mz=self.max_mz,
-                    min_intensity=self.min_intensity,
-                    fragment_tol_mass=self.fragment_tol_mass,
-                )
+            make_dataset = functools.partial(
+                AnnotatedSpectrumDataset if annotated else SpectrumDataset,
+                n_peaks=self.n_peaks,
+                min_mz=self.min_mz,
+                max_mz=self.max_mz,
+                min_intensity=self.min_intensity,
+                remove_precursor_tol=self.remove_precursor_tol,
+            )
             if self.test_index is not None:
                 self.test_dataset = make_dataset(self.test_index)
 
-    def _make_loader(self, dataset):
-        """Create a PyTorch DataLoader.
+    def _make_loader(
+        self, dataset: torch.utils.data.Dataset
+    ) -> torch.utils.data.DataLoader:
+        """
+        Create a PyTorch DataLoader.
 
         Parameters
         ----------
@@ -150,7 +136,7 @@ class DeNovoDataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             collate_fn=prepare_batch,
             pin_memory=True,
-            num_workers=self.num_workers,
+            num_workers=self.n_workers,
         )
 
     def train_dataloader(self):
@@ -166,30 +152,38 @@ class DeNovoDataModule(pl.LightningDataModule):
         return self._make_loader(self.test_dataset)
 
 
-def prepare_batch(batch):
-    """This is the collate function
+def prepare_batch(
+    batch: List[Tuple[torch.Tensor, float, int, str]]
+) -> Tuple[torch.Tensor, torch.Tensor, np.ndarray]:
+    """
+    Collate MS/MS spectra into a batch.
 
-    The mass spectra must be padded so that they fit nicely as a tensor.
-    However, the padded elements are ignored during the subsequent steps.
+    The MS/MS spectra will be padded so that they fit nicely as a tensor. However, the
+    padded elements are ignored during the subsequent steps.
 
     Parameters
     ----------
-    batch : tuple of tuple of torch.Tensor
-        A batch of data from an AnnotatedSpectrumDataset.
+    batch : List[Tuple[torch.Tensor, float, int, str]]
+        A batch of data from an AnnotatedSpectrumDataset, consisting of for each
+        spectrum (i) a tensor with the m/z and intensity peak values, (ii), the
+        precursor m/z, (iii) the precursor charge, (iv) the spectrum identifier.
 
     Returns
     -------
     spectra : torch.Tensor of shape (batch_size, n_peaks, 2)
-        The mass spectra to sequence, where ``X[:, :, 0]`` are the m/z values
-        and ``X[:, :, 1]`` are their associated intensities.
+        The padded mass spectra tensor with the m/z and intensity peak values for each
+        spectrum.
     precursors : torch.Tensor of shape (batch_size, 2)
-        The precursor mass and charge state.
-    sequence_or_ids : list of str
-        The peptide sequence annotations in training, the spectrum identifier in de novo sequencing
+        A tensor with the precursor neutral mass and precursor charge.
+    spectrum_ids : np.ndarray
+        The spectrum identifiers (during de novo sequencing) or peptide sequences
+        (during training).
     """
-    spec, mz, charge, sequence_or_ids = list(zip(*batch))
-    charge = torch.tensor(charge)
-    mass = (torch.tensor(mz) - 1.007276) * charge
-    precursors = torch.vstack([mass, charge]).T.float()
-    spec = torch.nn.utils.rnn.pad_sequence(spec, batch_first=True)
-    return spec, precursors, np.array(sequence_or_ids)
+    spectra, precursor_mzs, precursor_charges, spectrum_ids = list(zip(*batch))
+    spectra = torch.nn.utils.rnn.pad_sequence(spectra, batch_first=True)
+    precursor_charges = torch.tensor(precursor_charges)
+    precursor_masses = (
+        torch.tensor(precursor_mzs) - 1.007276
+    ) * precursor_charges
+    precursors = torch.vstack([precursor_masses, precursor_charges]).T.float()
+    return spectra, precursors, np.asarray(spectrum_ids)
