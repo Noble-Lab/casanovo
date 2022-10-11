27838b8dcaa52bb28a60a65b1fdcf1a87377d675
William Fondrie <fondriew@gmail.com>
2022-10-11 15:38:13 -0700

    Monkeypatch loaded version

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,7 +4,7 @@ from casanovo import casanovo
 
 def test_denovo(mgf_small, tmp_path, monkeypatch):
     # We can use this to explicitly test different versions.
-    monkeypatch.setattr("casanovo.__version__", "3.0.1")
+    monkeypatch.setattr(casanovo, "__version__", "3.0.1")
 
     # Predict on a small MGF file and verify that the output file exists.
     output_filename = tmp_path / "test.mztab"
