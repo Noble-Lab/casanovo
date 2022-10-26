80130054d190dede678a0eb7400269ef5151561f
Melih Yilmaz <32707537+melihyilmaz@users.noreply.github.com>
2022-10-26 09:52:10 -0700

    Minor style and import fix

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,17 +1,19 @@
 """Test that setuptools-scm is working correctly"""
 import casanovo
-from ..denovo.model import Spec2Pep
+from casanovo.denovo.model import Spec2Pep
+
 
 def test_version():
     """Check that the version is not None"""
     assert casanovo.__version__ is not None
-    
+
+
 def test_tensorboard():
     """Check that the version is not None"""
     model = Spec2Pep(
         tb_summarywriter="test_path",
     )
     assert model.tb_summarywriter is not None
-    
+
     model = Spec2Pep()
     assert model.tb_summarywriter is None
