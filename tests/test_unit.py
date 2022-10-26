532326b62cdbd7a6f71084b86f3f9b9bba91eb59
Melih Yilmaz <32707537+melihyilmaz@users.noreply.github.com>
2022-10-26 09:43:32 -0700

    Add tensorboard unit test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,7 +1,17 @@
 """Test that setuptools-scm is working correctly"""
 import casanovo
-
+from ..denovo.model import Spec2Pep
 
 def test_version():
     """Check that the version is not None"""
     assert casanovo.__version__ is not None
+    
+def test_tensorboard():
+    """Check that the version is not None"""
+    model = Spec2Pep(
+        tb_summarywriter="test_path",
+    )
+    assert model.tb_summarywriter is not None
+    
+    model = Spec2Pep()
+    assert model.tb_summarywriter is None
