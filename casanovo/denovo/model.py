8fb5ff93312a1343c3fd130ebe97e93f8efbf4be
Wout Bittremieux <wout@bittremieux.be>
2023-03-09 09:33:26 +0100

    Fix minimum peptide length

--- casanovo/denovo/model.py
+++ casanovo/denovo/model.py
@@ -98,7 +98,7 @@ class Spec2Pep(pl.LightningModule, ModelMixin):
         max_charge: int = 5,
         precursor_mass_tol: float = 50,
         isotope_error_range: Tuple[int, int] = (0, 1),
-        min_peptide_len: int = 8,
+        min_peptide_len: int = 6,
         n_beams: int = 5,
         n_log: int = 10,
         tb_summarywriter: Optional[
