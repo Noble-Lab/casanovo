9cd2770d108131a5ad83a6010a235e6d36f276d9
William Fondrie <fondriew@gmail.com>
2023-05-09 23:45:16 -0700

    Add runner tests

--- tests/unit_tests/test_runner.py
+++ tests/unit_tests/test_runner.py
@@ -1,7 +1,10 @@
 """Unit tests specifically for the model_runner module."""
-from pathlib import Path
+from typing import Union, Any, Dict
 
+import lightning.pytorch as pl
 import pytest
+import torch
+from lightning.pytorch.accelerators import Accelerator
 
 from casanovo.config import Config
 from casanovo.denovo.model_runner import ModelRunner
@@ -41,3 +44,36 @@ def test_initialize_model(tmp_path):
     with pytest.raises(EOFError):
         runner = ModelRunner(config=config, model_filename=str(weights))
         runner.initialize_model(train=False)
+
+
+def test_save_and_load_weights(tmp_path, mgf_small, tiny_config):
+    """Test saving aloading weights"""
+    config = Config(tiny_config)
+    config.max_epochs = 1
+    config.n_layers = 1
+    ckpt = tmp_path / "test.ckpt"
+
+    with ModelRunner(config=config) as runner:
+        runner.train([mgf_small], [mgf_small])
+        runner.trainer.save_checkpoint(ckpt)
+
+    # Try changing model arch:
+    config.n_layers = 50  # lol
+    with torch.device("meta"):
+        # Now load the weights into a new model
+        # The device should be meta for all the weights.
+        runner = ModelRunner(config=config, model_filename=ckpt)
+        runner.initialize_model(train=False)
+
+    obs_layers = runner.model.encoder.transformer_encoder.num_layers
+    assert obs_layers == 1  # Mach the original arch.
+    assert next(runner.model.parameters()).device == torch.device("meta")
+
+    # If the Trainer correctly moves the weights to the accelerator,
+    # then it should fail if the weights are on the "meta" device.
+    with torch.device("meta"):
+        with ModelRunner(config=config, model_filename=ckpt) as runner:
+            with pytest.raises(NotImplementedError) as err:
+                runner.evaluate([mgf_small])
+
+    assert "meta tensor; no data!" in str(err.value)
