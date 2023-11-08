21308c2c91ea18071469fc2544813fb3797ac73b
cfmelend <cfmelend@gmail.com>
2023-11-08 13:14:12 -0800

    Add resume_training option.

--- casanovo/denovo/model_runner.py
+++ casanovo/denovo/model_runner.py
@@ -103,11 +103,19 @@ class ModelRunner:
         self.initialize_data_module(train_index, valid_index)
         self.loaders.setup()
 
-        self.trainer.fit(
-            self.model,
-            self.loaders.train_dataloader(),
-            self.loaders.val_dataloader(),
-        )
+        if self.config.resume_training:
+            self.trainer.fit(
+                self.model,
+                self.loaders.train_dataloader(),
+                self.loaders.val_dataloader(),
+                ckpt_path=self.model_filename,
+            )
+        else:
+            self.trainer.fit(
+                self.model,
+                self.loaders.train_dataloader(),
+                self.loaders.val_dataloader(),
+            )
 
     def evaluate(self, peak_path: Iterable[str]) -> None:
         """Evaluate peptide sequence preditions from a trained Casanovo model.
