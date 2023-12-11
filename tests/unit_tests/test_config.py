e073415109b71d205d0f36d0c4c32f4feca3ccea
Wout Bittremieux <bittremieux@users.noreply.github.com>
2023-12-11 11:54:07 +0100

    Check for invalid/missin config entries (#268)
    * Refactor config checking
    
    - More efficient checking for unknown and missing config entries (no needless conversions/creations of sets and lists).
    - Avoid creating temporary files in root directories during unit testing.
    - Update outdated test.
    
    * Add unit test fix

--- tests/unit_tests/test_config.py
+++ tests/unit_tests/test_config.py
@@ -1,7 +1,8 @@
 """Test configuration loading"""
-from casanovo.config import Config
 import pytest
 import yaml
+
+from casanovo.config import Config
 
 
 def test_default():
@@ -14,27 +15,24 @@ def test_default():
 
 
 def test_override(tmp_path, tiny_config):
-    """Test overriding the default"""
-    yml = tmp_path / "test.yml"
-    with yml.open("w+") as f_out:
-        f_out.write(
-            """random_seed: 42
-top_match: 3
-residues:
-  W: 1
-  O: 2
-  U: 3
-  T: 4
-"""
-        )
+    # Test expected config option is missing.
+    filename = str(tmp_path / "config_missing.yml")
+    with open(tiny_config, "r") as f_in, open(filename, "w") as f_out:
+        cfg = yaml.safe_load(f_in)
+        # Remove config option.
+        del cfg["random_seed"]
+        yaml.safe_dump(cfg, f_out)
 
-    with open(tiny_config, "r") as read_file:
-        contents = yaml.safe_load(read_file)
-        contents["random_seed_"] = 354
+    with pytest.raises(KeyError):
+        Config(filename)
 
-    with open("output.yml", "w") as write_file:
-        yaml.safe_dump(contents, write_file)
+    # Test invalid config option is present.
+    filename = str(tmp_path / "config_invalid.yml")
+    with open(tiny_config, "r") as f_in, open(filename, "w") as f_out:
+        cfg = yaml.safe_load(f_in)
+        # Insert invalid config option.
+        cfg["random_seed_"] = 354
+        yaml.safe_dump(cfg, f_out)
+
     with pytest.raises(KeyError):
-        config = Config("output.yml")
-    with pytest.raises(KeyError):
-        config = Config(yml)
+        Config(filename)
