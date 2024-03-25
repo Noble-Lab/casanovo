43f01ada60ba2eca39fc428fab7f93b210d90f74
Wout Bittremieux <bittremieux@users.noreply.github.com>
2024-03-25 18:41:26 +0100

    Fix linting issue

--- casanovo/data/ms_io.py
+++ casanovo/data/ms_io.py
@@ -147,7 +147,7 @@ class MztabWriter:
         """
         Export the spectrum identifications to the mzTab file.
         """
-        with open(self.filename, "w", newline='') as f:
+        with open(self.filename, "w", newline="") as f:
             writer = csv.writer(f, delimiter="\t", lineterminator=os.linesep)
             # Write metadata.
             for row in self.metadata:
