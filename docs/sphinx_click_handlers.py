2ac9eabc757b92ca40238d68d62e7ec02d16b89e
Lilferrit <straub.gavin@gmail.com>
2024-07-09 09:57:27 -0700

    requested changes

--- docs/sphinx_click_handlers.py
+++ docs/sphinx_click_handlers.py
@@ -2,8 +2,6 @@ def process_description(app, ctx, lines):
     if ctx.command.name == "main":
         del lines[:3]
 
-    print(lines)
-
 
 def setup(app):
     app.connect("sphinx-click-process-description", process_description)
