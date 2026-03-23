0a5ef3ff9e542b1f5b69648321edea82328971cd
Bhavya Krishnan <bhavyak7@cysteine.gs.washington.edu>
2026-03-23 09:49:46 -0700

    Fixed linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -81,7 +81,7 @@ def _create_mgf(
     entries = [
         _create_mgf_entry(
             p,
-            str(i),
+            i,
             rng.choice([2, 3]),
             mod_aa_mass=mod_aa_mass,
             annotate=annotate,
