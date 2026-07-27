8f5157df7bf81467a373db157a1091c97fe30eef
Bhavya Krishnan <bhavyak7@cysteine.gs.washington.edu>
2026-07-27 15:32:04 -0700

    initial commit

--- casanovo/denovo/dataloaders.py
+++ casanovo/denovo/dataloaders.py
@@ -7,8 +7,10 @@ import os
 import pathlib
 from typing import Optional, Sequence
 
+import lance
 import lightning.pytorch as pl
 import numpy as np
+import pyteomics
 import pyarrow as pa
 import spectrum_utils.spectrum as sus
 import torch.utils.data._utils.collate
@@ -136,6 +138,8 @@ class DeNovoDataModule(pl.LightningDataModule):
         shuffle: Optional[bool] = True,
         shuffle_buffer_size: Optional[int] = 10_000,
         n_workers: Optional[int] = None,
+        ms_level: Optional[int] = 2,
+        scan_width: Optional[int] = 2,
     ):
         super().__init__()
 
@@ -159,6 +163,8 @@ class DeNovoDataModule(pl.LightningDataModule):
             _scale_to_unit_norm,
         ]
         self.valid_charge = np.arange(1, max_charge + 1)
+        self.ms_level = ms_level
+        self.scan_width = scan_width
 
         self.tokenizer = tokenizer or PeptideTokenizer()
 
@@ -302,6 +308,7 @@ class DeNovoDataModule(pl.LightningDataModule):
             preprocessing_fn=self.preprocessing_fn,
             valid_charge=self.valid_charge,
             custom_fields=custom_fields,
+            ms_level=self.ms_level,
         )
 
         dataset_params = dict(
@@ -327,6 +334,12 @@ class DeNovoDataModule(pl.LightningDataModule):
         ):
             dataset = Dataset.from_lance(paths[0], **params)
         else:
+            if not all(
+                (pathlib.Path(path).suffix.lower() == ".mzml")
+                for path in paths
+            ) and (self.ms_level == 1):
+                paths = self._dia_to_dataframe(paths, annotated)
+
             dataset = Dataset(
                 spectra=paths,
                 path=lance_path,
@@ -341,6 +354,288 @@ class DeNovoDataModule(pl.LightningDataModule):
             dataset = ShufflerIterDataPipe(dataset, buffer_size=buffer_batches)
 
         return dataset
+
+    def _dia_to_dataframe(self, paths, annotated):
+        for spectra in paths:
+            f_to_mzrt_to_pep, max_mz, window_size, cycle_time = (
+                self.get_centers(spectra)
+            )
+            for part in f_to_mzrt_to_pep.keys():
+                prec_to_spec = self._extract_spectra(
+                    spectra,
+                    f_to_mzrt_to_pep,
+                    part,
+                    self.max_peaks,
+                    (self.scan_width + 1) * cycle_time,
+                    max_mz,
+                )
+                for key, value in prec_to_spec.items():
+                    prec, rt, _ = key
+                    if "ms1_scans" not in value:
+                        skipped += 1
+                        continue
+
+                    scans = np.array(value["scans"], dtype=object)
+                    rts = np.array(value["rts"])
+                    ms1_scans = np.array(value["ms1_scans"], dtype=object)
+                    ms1_rts = np.array(value["ms1_rts"])
+                    window_width = value["window_width"]
+
+                    abs_rts = [np.abs(x) for x in rts]
+                    sorted_rt_idxs = np.argsort(abs_rts)[: self.scan_width]
+                    rts = rts[sorted_rt_idxs]
+                    scans = scans[sorted_rt_idxs]
+
+                    abs_ms1_rts = [np.abs(x) for x in ms1_rts]
+                    sorted_ms1_rt_idxs = np.argsort(abs_ms1_rts)[
+                        : self.scan_width
+                    ]
+                    ms1_rts = ms1_rts[sorted_ms1_rt_idxs]
+                    ms1_scans = ms1_scans[sorted_ms1_rt_idxs]
+
+                    for charge in range(1, self.max_charge + 1):
+                        mz_array = []
+                        intensity_array = []
+                        scan_window_array = []
+                        ms_array = []
+
+                        for scan, cur_rt in zip(scans, rts):
+                            for mz, intensity in scan:
+                                mz_array.append(mz)
+                                intensity_array.append(intensity)
+                                scan_window_array.append(cur_rt)
+                                ms_array.append(2)
+
+                        for scan, cur_rt in zip(ms1_scans, ms1_rts):
+                            for mz, intensity in scan:
+
+                                if abs(mz - prec) > window_width + 1:
+                                    continue
+
+                                mz_array.append(mz)
+                                intensity_array.append(intensity)
+                                scan_window_array.append(cur_rt)
+                                ms_array.append(1)
+
+                        record = {
+                            "peak_file": pathlib.Path(spectra).name,
+                            "scan_id": value["center_scan_id"],
+                            "precursor_mz": prec,
+                            "mz_array": np.asarray(mz_array, dtype=np.float32),
+                            "intensity_array": np.asarray(
+                                intensity_array, dtype=np.float32
+                            ),
+                            "scan_window_array": np.asarray(
+                                scan_window_array, dtype=np.float32
+                            ),
+                            "ms_array": np.asarray(ms_array, dtype=np.int8),
+                        }
+
+                        if not annotated:
+                            record["charge"] = charge
+
+                        yield pl.DataFrame(record)
+
+            logging.warning(
+                f"{skipped} spectra were skipped due to missing MS1 scans for file {spectra}"
+            )
+
+    def _extract_spectra(
+        mzml_file, f_to_mzrt_to_pep, part, top_n, time_width, max_mz
+    ):
+        prec_to_spec = {}
+        with pyteomics.mzml.read(mzml_file) as reader:
+            for spec in reader:
+                cur_rt = 60 * spec["scanList"]["scan"][0]["scan start time"]
+                if spec["ms level"] == 1:
+                    for scan_rt in range(
+                        int(cur_rt / 10) - 1, int(cur_rt / 10) + 1
+                    ):
+                        for scan_window in range(max_mz + 1):
+                            if (scan_window, scan_rt) in f_to_mzrt_to_pep[
+                                part
+                            ]:
+                                for mz, rt, charge in f_to_mzrt_to_pep[part][
+                                    (scan_window, scan_rt)
+                                ]:
+                                    if np.abs(rt - cur_rt) < time_width:
+                                        mzs = spec["m/z array"]
+                                        intensities = spec["intensity array"]
+
+                                        sorted_intensity_idxs = np.argsort(
+                                            intensities
+                                        )[-top_n:]
+                                        intensities = intensities[
+                                            sorted_intensity_idxs
+                                        ]
+                                        mzs = mzs[sorted_intensity_idxs]
+
+                                        sorted_mz_idxs = np.argsort(mzs)
+                                        intensities = intensities[
+                                            sorted_mz_idxs
+                                        ]
+                                        mzs = mzs[sorted_mz_idxs]
+
+                                        intensities = intensities**0.5
+                                        if len(intensities) > 0:
+                                            intensities = intensities / np.max(
+                                                intensities
+                                            )
+
+                                        if (
+                                            mz,
+                                            rt,
+                                            charge,
+                                        ) not in prec_to_spec:
+                                            prec_to_spec[(mz, rt, charge)] = {
+                                                "center_scan_id": spec.get(
+                                                    "params"
+                                                ).get("id"),
+                                            }
+                                        if (
+                                            "ms1_scans"
+                                            not in prec_to_spec[
+                                                (mz, rt, charge)
+                                            ]
+                                        ):
+                                            prec_to_spec[(mz, rt, charge)][
+                                                "ms1_scans"
+                                            ] = []
+                                            prec_to_spec[(mz, rt, charge)][
+                                                "ms1_rts"
+                                            ] = []
+                                        prec_to_spec[(mz, rt, charge)][
+                                            "ms1_scans"
+                                        ].append(
+                                            [x for x in zip(mzs, intensities)]
+                                        )
+                                        prec_to_spec[(mz, rt, charge)][
+                                            "ms1_rts"
+                                        ].append(cur_rt - rt)
+                elif spec["ms level"] == 2:
+                    window = spec["precursorList"]["precursor"][0][
+                        "isolationWindow"
+                    ]
+                    window_center = window["isolation window target m/z"]
+                    lower_offset = window["isolation window lower offset"]
+                    upper_offset = window["isolation window upper offset"]
+
+                    for scan_rt in range(
+                        int(cur_rt / 10) - 1, int(cur_rt / 10) + 1
+                    ):
+                        for scan_window in range(
+                            int((window_center - lower_offset) / 10) - 1,
+                            int((window_center + upper_offset) / 10) + 1,
+                        ):
+                            if (scan_window, scan_rt) in f_to_mzrt_to_pep[
+                                part
+                            ]:
+                                for mz, rt, charge in f_to_mzrt_to_pep[part][
+                                    (scan_window, scan_rt)
+                                ]:
+                                    in_mz = (
+                                        mz > window_center - lower_offset
+                                        and mz < window_center + upper_offset
+                                    )
+                                    rt_diff = np.abs(rt - cur_rt)
+                                    if in_mz and rt_diff < time_width:
+                                        mzs = spec["m/z array"]
+                                        intensities = spec["intensity array"]
+
+                                        sorted_intensity_idxs = np.argsort(
+                                            intensities
+                                        )[-top_n:]
+                                        intensities = intensities[
+                                            sorted_intensity_idxs
+                                        ]
+                                        mzs = mzs[sorted_intensity_idxs]
+
+                                        sorted_mz_idxs = np.argsort(mzs)
+                                        intensities = intensities[
+                                            sorted_mz_idxs
+                                        ]
+                                        mzs = mzs[sorted_mz_idxs]
+
+                                        intensities = intensities**0.5
+                                        if len(intensities) > 0:
+                                            intensities = intensities / np.max(
+                                                intensities
+                                            )
+
+                                        if (
+                                            mz,
+                                            rt,
+                                            charge,
+                                        ) not in prec_to_spec:
+                                            prec_to_spec[(mz, rt, charge)] = {}
+                                        if (
+                                            "scans"
+                                            not in prec_to_spec[
+                                                (mz, rt, charge)
+                                            ]
+                                        ):
+                                            prec_to_spec[(mz, rt, charge)][
+                                                "scans"
+                                            ] = []
+                                            prec_to_spec[(mz, rt, charge)][
+                                                "rts"
+                                            ] = []
+                                            prec_to_spec[(mz, rt, charge)][
+                                                "window_width"
+                                            ] = max(lower_offset, upper_offset)
+                                        prec_to_spec[(mz, rt, charge)][
+                                            "scans"
+                                        ].append(
+                                            [x for x in zip(mzs, intensities)]
+                                        )
+                                        prec_to_spec[(mz, rt, charge)][
+                                            "rts"
+                                        ].append(cur_rt - rt)
+        return prec_to_spec
+
+    def _get_centers(mzml_file):
+        f_to_mzrt_to_pep = {}
+        max_mz = 0
+        num_spectra = 0
+        part = 0
+        last_rt = 0
+        cycle_time = None
+        with pyteomics.mzml.read(mzml_file, decode_binary=False) as reader:
+            for spec in reader:
+                if spec["ms level"] == 1:
+                    cur_rt = (
+                        60 * spec["scanList"]["scan"][0]["scan start time"]
+                    )
+                    cycle_time = cur_rt - last_rt
+                    last_rt = cur_rt
+                if spec["ms level"] == 2:
+                    window = spec["precursorList"]["precursor"][0][
+                        "isolationWindow"
+                    ]
+                    window_center = window["isolation window target m/z"]
+                    lower_offset = window["isolation window lower offset"]
+                    upper_offset = window["isolation window upper offset"]
+                    window_size = upper_offset + lower_offset
+                    cur_rt = (
+                        60 * spec["scanList"]["scan"][0]["scan start time"]
+                    )
+                    if num_spectra % 50000 == 0:
+                        part += 1
+                        f_to_mzrt_to_pep[part] = {}
+
+                    num_spectra += 1
+                    key = (int(window_center / 10), int(cur_rt / 10))
+                    max_mz = max(max_mz, int(window_center / 10))
+                    if key in f_to_mzrt_to_pep[part]:
+                        f_to_mzrt_to_pep[part][key].append(
+                            (window_center, cur_rt, 1)
+                        )
+                    else:
+                        f_to_mzrt_to_pep[part][key] = [
+                            (window_center, cur_rt, 1)
+                        ]
+
+        return f_to_mzrt_to_pep, max_mz, window_size, cycle_time
 
     def _make_loader(
         self, dataset: torch.utils.data.Dataset, shuffle: bool = False
